5c6e0214fd7409588350d2a0a31002ed9efe1028
Shadrach Godwin <shadrachgodwin@gmail.com>
2025-08-19 14:08:00 +0100

    update

--- .history/vite.config_20250819134542.js
+++ .history/vite.config_20250819134542.js
@@ -4,15 +4,14 @@ import laravel from 'laravel-vite-plugin'
 export default defineConfig({
   plugins: [
     laravel({
-      input: [
-        'resources/css/app.css',
-        'resources/js/app.js'
-      ],
+      input: ['resources/css/app.css', 'resources/js/app.js'],
       refresh: true,
     }),
+    tailwindcss(),
   ],
   build: {
     outDir: 'public/build',
     emptyOutDir: true,
   },
 })
+
